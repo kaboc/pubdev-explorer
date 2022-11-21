3a74c3ff5867e644de4e9a398f2521131d97a818
Kabo <20254485+kaboc@users.noreply.github.com>
2022-11-21 16:37:47 +0900

    Tiny improvements

--- core/lib/src/common/logger.dart
+++ core/lib/src/common/logger.dart
@@ -1,9 +1,9 @@
-import 'dart:developer' show log;
+import 'dart:developer' as develop show log;
 
 // ignore: avoid_classes_with_only_static_members
 class Logger {
   static void error(Object? error, [StackTrace? stack]) {
     final len = error.toString().length + 4;
-    log('-' * len, error: error, stackTrace: stack);
+    develop.log('-' * len, error: error, stackTrace: stack);
   }
 }
