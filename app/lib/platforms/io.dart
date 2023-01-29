1f84237edee97dd192890a0df33c0f23d804742c
Kabo <20254485+kaboc@users.noreply.github.com>
2023-01-29 17:39:09 +0900

    Add notes about database path and App Inspection

--- app/lib/platforms/io.dart
+++ app/lib/platforms/io.dart
@@ -11,8 +11,15 @@ LazyDatabase openConnection({bool inMemory = false}) {
       return NativeDatabase.memory();
     }
 
+    // Another way is to use getDatabasesPath() of sqflite.
+    // On Android, it is probably better as it return the
+    // default database path.
     final dirPath = await _getAppSupportDirPath();
     final file = File(path.join(dirPath, 'app.sqlite'));
+
+    // App Inspection on Android Studio/IntelliJ Idea does
+    // not support database opened with NativeDatabase.
+    // https://github.com/simolus3/drift/discussions/1818
     return NativeDatabase(file);
   });
 }
