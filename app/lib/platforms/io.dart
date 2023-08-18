7e613dd8f31c5769714cae8a6b4926ddaaa19817
Kabo <20254485+kaboc@users.noreply.github.com>
2023-08-18 22:02:31 +0900

    Improve imports

--- app/lib/platforms/io.dart
+++ app/lib/platforms/io.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
+import 'package:drift/drift.dart' show LazyDatabase;
+import 'package:drift/native.dart' show NativeDatabase;
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 
--- app/lib/platforms/web.dart
+++ app/lib/platforms/web.dart
@@ -1,5 +1,5 @@
-import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
+import 'package:drift/drift.dart' show LazyDatabase;
+import 'package:drift/web.dart' show WebDatabase;
 
 LazyDatabase openConnection({bool inMemory = false}) {
   return LazyDatabase(() => WebDatabase('app'));
